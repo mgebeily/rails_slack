52826b6f97703f4a7c011566f9be955b36ac99bc
Marc Gebeily <m.gebeily@M.Gebeily-C02ZW87KMD6R>
2023-03-12 16:44:42 -0400

    Add slack team helper

--- app/models/rails_slack/team.rb
+++ app/models/rails_slack/team.rb
@@ -8,14 +8,15 @@ module RailsSlack
       @slack_client ||= ::Slack::Web::Client.new(token: token)
     end
 
-    def send_message(channel_id, locals = {}, message = {})
+    def send_message(channel, message, locals = {})
       response = ApplicationController.new.render_to_string(
         template: "rails_slack/messages/#{message}",
         # TODO: This is deprecated in Rails 7
-        locals: locals.transform_keys { |x| x.starts_with?('@') ? x : "@#{x}" }
+        locals: locals.transform_keys { |x| x.starts_with?('@') ? x : "@#{x}".to_sym },
+        formats: [:json]
       )
 
-      slack_client.chat_postMessage(channel:, blocks: JSON.parse(response))
+      slack_client.chat_postMessage(channel:, blocks: JSON.parse(response)['blocks'])
     end
   end
 end
